4f2b9df291b34921709303d26c16cd2cffd052f6
Diego Silva <diegosilva157890@gmail.com>
2023-09-07 22:51:21 -0300

    Tela de Listagem Map

--- client/src/ListagemEquipamento/index.tsx
+++ client/src/ListagemEquipamento/index.tsx
@@ -1,40 +1,51 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, FlatList } from "react-native";
 import styles from "./style";
 
 export default function ListaEquipamento() {
+    const data = [
+        {
+          id: 1,
+          image: require('../assets/iconImage.png'),
+          text1: "Transformador #T1000",
+          text2: "21212 01:02 - 12:54",
+        },
+        {
+          id: 2,
+          image: require('../assets/iconImage.png'),
+          text1: "Transformador #T1001",
+          text2: "21213 02:03 - 13:55",
+        },
+        {
+          id: 3,
+          image: require('../assets/iconImage.png'),
+          text1: "Transformador #T1002",
+          text2: "21214 03:04 - 14:56",
+        },
+        {
+          id: 4,
+          image: require('../assets/iconImage.png'),
+          text1: "Transformador #T1003",
+          text2: "21215 04:05 - 15:57",
+        },
 
 
-      
+      ];
   return (
-    <View>
-      <View style={styles.container}>
-        <View style={styles.row}>
-          <View style={styles.column}>
-            <Image source={require('../assets/iconImage.png')} style={styles.image} />
-            <Text style={styles.textfont}>Transformador #T1000</Text>
-            <Text>21212 01:02 - 12:54</Text>
-          </View>
-          <View style={styles.column}>
-            <Image source={require('../assets/iconImage.png')} style={styles.image} />
-            <Text style={styles.textfont}>Transformador #T1001</Text>
-            <Text>21213 02:03 - 13:55</Text>
-          </View>
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id.toString()}
+      numColumns={2}
+      renderItem={({ item }) => (
+        <View style={styles.column}>
+          <Image source={item.image} style={styles.image} />
+          <Text style={styles.textfont}>{item.text1}</Text>
+          <Text>{item.text2}</Text>
         </View>
-
-        <View style={styles.row}>
-          <View style={styles.column}>
-            <Image source={require('../assets/iconImage.png')} style={styles.image} />
-            <Text style={styles.textfont}>Transformador #T1002</Text>
-            <Text>21214 03:04 - 14:56</Text>
-          </View>
-          <View style={styles.column}>
-            <Image source={require('../assets/iconImage.png')} style={styles.image} />
-            <Text style={styles.textfont}>Transformador #T1003</Text>
-            <Text>21215 04:05 - 15:57</Text>
-          </View>
-        </View>
-      </View>
-    </View>
+      )}
+      contentContainerStyle={styles.container}
+    />
   );
+  
 }
+
--- client/src/ListagemEquipamento/style.ts
+++ client/src/ListagemEquipamento/style.ts
@@ -2,29 +2,34 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     container: {
-      flexDirection: "column",
-      paddingHorizontal: 10,
-      marginBottom: 10,
-    },
-    row: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      marginBottom: 10,
-    },
-    column: {
-      flex: 1,
-      marginRight: 10,
-      alignItems: "center", // Centraliza verticalmente o conteúdo na coluna
-    },
-    image: {
-      width: "100%",
-      height: 100, // Ajuste conforme necessário
-      resizeMode: "contain",
-    },
-    textfont: {
-      fontWeight: "bold",
-    },
-  });
+        flexDirection: "column",
+        paddingHorizontal: 10,
+        marginBottom: 10,
+      },
+      row: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginBottom: 10,
+      },
+      column: {
+        flex: 1,
+        marginRight: 5, 
+        alignItems: "center", 
+      },
+      column2: {
+        flex: 1,
+        marginLeft: 5, 
+        alignItems: "center", 
+      },
+      image: {
+        width: "100%",
+        height: 100,
+        resizeMode: "contain",
+      },
+      textfont: {
+        fontWeight: "bold",
+      },
+    });
   
   export default styles
 
