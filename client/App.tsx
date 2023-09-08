04277acb43f73a7f24b1533dfc55016143187e1c
Diego Silva <diegosilva157890@gmail.com>
2023-09-08 12:05:58 -0300

    Telas Com a Navigation

--- client/App.tsx
+++ client/App.tsx
@@ -1,13 +1,15 @@
 import { StyleSheet, ImageBackground, View, Image } from 'react-native';
-import Main from './src/components/Main';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigation from './src/components/Navigation';
+import NavBar from './src/components/NavBar';
 
 
 export default function App() {
-  return (
+  return (     
     <NavigationContainer>
-        <Navigation/>
+      <NavBar/>
+      <Navigation/>
     </NavigationContainer>
+    
   );
 }
--- client/src/components/Main/style.ts
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from "react-native";
-
-const styles = StyleSheet.create({
-    Textfont: {
-      fontSize: 28
-    }
-  });
-   
-  export default styles
