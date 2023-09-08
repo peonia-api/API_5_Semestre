6159757fef7d327398f35633221ca925e481b85d
Diego Silva <diegosilva157890@gmail.com>
2023-09-08 10:56:23 -0300

    Navigation Atualização

--- client/src/components/Navigation/index.tsx
+++ client/src/components/Navigation/index.tsx
@@ -1,49 +1,57 @@
 import React from 'react';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { createAppContainer } from 'react-navigation';
-import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
-
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import Cadastro from '../Cadastro';
 import ListaEquipamento from '../ListagemEquipamento';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Detalhe from '../Detalhe';
 
 Icon.loadFont();
 
-const Tab = createMaterialBottomTabNavigator(
-  {
-    Cadastro: {
-      screen: Cadastro,
-      navigationOptions: {
-        tabBarLabel: 'Cadastro',
-        tabBarIcon: ({ focused }: any) => (
-          <Icon name="lock" size={25} color={focused ? '#fff' : '#ddd'} />
-        ),
-      },
-    },
-    Mapa: {
-      screen: Cadastro, // Substitua por sua tela de mapa
-      navigationOptions: {
-        tabBarLabel: 'Mapa',
-        tabBarIcon: ({ focused }: any) => (
-          <Icon name="map" size={25} color={focused ? '#fff' : '#ddd'} />
-        ),
-      },
-    },
-    ListaEquipamentos: {
-      screen: ListaEquipamento,
-      navigationOptions: {
-        tabBarLabel: 'Lista Equipamentos',
-        tabBarIcon: ({ focused }: any) => (
-          <Icon name="list" size={25} color={focused ? '#fff' : '#ddd'} />
-        ),
-      },
-    },
-  },
-  {
-    shifting: true,
-    activeColor: '#fff',
-    inactiveColor: '#ddd',
-    barStyle: { backgroundColor: '#694fad' },
-  }
-);
+const Tab = createMaterialBottomTabNavigator();
 
-export default createAppContainer(Tab);
+export default function Navigation() {
+  return (
+
+      <Tab.Navigator
+        initialRouteName="Cadastro"
+        activeColor="#000000"
+        shifting={false}
+        inactiveColor="#3e2465"
+        barStyle={{ backgroundColor: '#9a99ff' }}
+        sceneAnimationEnabled={false} 
+      >
+        <Tab.Screen
+          name="ListaEquipamento"
+          component={ListaEquipamento}
+          options={{
+            tabBarLabel: 'Lista Equipamento',
+            tabBarIcon: () => (
+              <Icon name="list" size={25} color="#000000" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Mapa"
+          component={Cadastro}
+          options={{
+            tabBarLabel: 'Mapa',
+            tabBarIcon: () => (
+              <Icon name="map-pin" size={25} color="#000000" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Detalhe"
+          component={Detalhe} 
+          options={{
+            tabBarLabel: 'Detalhe',
+            tabBarIcon: () => (
+              <Icon name="list" size={25} color="#000000" />
+            ),
+          }}
+        />
+        
+      </Tab.Navigator>
+   
+  );
+}
