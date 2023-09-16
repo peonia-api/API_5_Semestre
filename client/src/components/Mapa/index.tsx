046be7aa8a7f081a94c1f156576a43fa93e59403
Diego Silva <diegosilva157890@gmail.com>
2023-09-16 13:46:05 -0300

    Tela Mapa

--- client/src/components/Mapa/index.tsx
+++ client/src/components/Mapa/index.tsx
@@ -1,15 +1,70 @@
-import React from "react";
-import { View, Text, Image, Button } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Image, Button, TextInput, FlatList } from "react-native";
 import styles from "./style";
 import Pesquisa from "../Pesquisa";
+import { Picker } from "@react-native-picker/picker";
 
 export default function Mapa() {
+  const [selectedEquipa, setSelectedEquipa] = useState<string>('');
 
+  const handleEquipamentoChange = (equipamento: string) => {
+    setSelectedEquipa(equipamento);
+  };
+
+  const data = [
+    {
+      id: 1,
+      text1: "Transformador #T1000",
+      text2: "21215-321",
+    },
+    {
+      id: 2,
+      text1: "Transformador #T1001",
+      text2: "21215-321",
+    },
+    {
+      id: 3,
+      text1: "Transformador #T1002",
+      text2: "21215-321",
+    },
+    {
+      id: 4,
+      text1: "Transformador #T1003",
+      text2: "21215-321",
+    }
+  ];
 
   return (
-    <View >
-      
-      
+    <View style={styles.containerPrincipal}>
+      <View style={styles.containerTrans}>
+        <Picker
+          selectedValue={selectedEquipa}
+          onValueChange={handleEquipamentoChange}
+          style={styles.picker}
+        >
+          <Picker.Item label="Equipamento" value="" />
+          <Picker.Item label="TRANSFORMADOR" value="TRANSFORMADOR" />
+          <Picker.Item label="POSTE" value="POSTE" />
+          <Picker.Item label="BOMBA HIDRÁULICA" value="BOMBA HIDRÁULICA" />
+        </Picker>
+      </View>
+
+      <View>
+        <Text>Mapa</Text>
+      </View>
+
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.itemContainer}>
+            <View style={styles.item}>
+              <Text style={styles.textfont}>{item.text1}</Text>
+              <Text>{item.text2}</Text>
+            </View>
+          </View>
+        )}
+      />
     </View>
   );
 }
--- client/src/components/Mapa/style.ts
+++ client/src/components/Mapa/style.ts
@@ -1,10 +1,43 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#ffffff'
+  containerPrincipal: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    paddingTop: 10,
   },
-  });
+  picker: {
+    width: "95%",
+    height: 50,
+    backgroundColor: "#f2f2f2",
+    marginBottom: 15,
+    alignSelf: "center",
+    
+  },
+  textfont: {
+    fontWeight: "bold",
+    fontSize: 19,
+  },
+  itemContainer: {
+    flexDirection: "column",
+    marginBottom: 10,
+    width: "95%",
+    alignSelf: "center",
 
-   
-  export default styles;
+  },
+  containerTrans: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 20,
+    display: "flex",
+  },
+  item: {
+    width: "95%",
+    margin: 5,
+    height: 'auto',
+    backgroundColor: "#f2f2f2",
+    borderRadius: 5
+  },
+});
+
+export default styles;
