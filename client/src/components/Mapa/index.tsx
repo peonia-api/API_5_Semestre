0967d31c7beee255b6c01d9028ec625bc464177d
Diego Silva <diegosilva157890@gmail.com>
2023-09-16 10:29:54 -0300

    Tela Map

---
+++ client/src/components/Mapa/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+import styles from "./style";
+import Pesquisa from "../Pesquisa";
+
+export default function NavBar() {
+  return (
+    <View>
+        <Text>teste</Text>
+    </View>
+  );
+}
--- client/src/components/Navigation/index.tsx
+++ client/src/components/Navigation/index.tsx
@@ -6,6 +6,7 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import Detalhe from '../Detalhe';
 import { NavigationContainer } from '@react-navigation/native';
 import NavBar from '../NavBar';
+import Mapa from '../Mapa';
 
 Icon.loadFont();
 
@@ -15,29 +16,28 @@ export default function Navigation() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        initialRouteName="Cadastro"
+        initialRouteName="Lista Equipamento"
         activeColor="#000000"
         shifting={false}
         inactiveColor="#3e2465"
         barStyle={{ backgroundColor: '#a4a3ff' }}
         sceneAnimationEnabled={false}
       >
-        <Tab.Screen
-          name="Detalhe"
-          component={Detalhe}
+      <Tab.Screen
+          name="Lista Equipamento"
+          component={ListaEquipamento}
           options={{
-            tabBarLabel: 'Detalhe',
+            tabBarLabel: 'Lista Equipamento',
             tabBarIcon: () => (
               <Icon name="list" size={25} color="#000000" />
             ),
           }}
         />
-
         <Tab.Screen
-          name="Cadastro"
-          component={Cadastro}
+          name="Mapa"
+          component={Mapa}
           options={{
-            tabBarLabel: 'Cadastro',
+            tabBarLabel: 'Mapa',
             tabBarIcon: () => (
               <Icon name="map" size={25} color="#000000" />
             ),
@@ -45,10 +45,10 @@ export default function Navigation() {
         />
 
         <Tab.Screen
-          name="Lista Equipamento"
-          component={ListaEquipamento}
+          name="Detalhe"
+          component={Detalhe}
           options={{
-            tabBarLabel: 'Lista Equipamento',
+            tabBarLabel: 'Detalhe',
             tabBarIcon: () => (
               <Icon name="list" size={25} color="#000000" />
             ),
