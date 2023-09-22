46f2959645d0a2b6b41e8e58e9d739a4da681601
mateushlsilva <mateushls01@gmail.com>
2023-09-22 15:25:51 -0300

    Adiciona ligação do mapa

--- client/src/components/Mapa/index.tsx
+++ client/src/components/Mapa/index.tsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
-import { View } from "react-native";
+import { View, Image } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import { requestForegroundPermissionsAsync, getCurrentPositionAsync, LocationObject } from "expo-location";
 import LottieView from 'lottie-react-native';
+import { useContextoEquipmente } from '../../hooks'
 
 export default function Mapa() {
   const [location, setLocation] = useState<LocationObject | null>(null);
   const [mapLoaded, setMapLoaded] = useState(false);
+  const { equipmente } = useContextoEquipmente()
 
+
+  
   async function requestLocationsPermissions() {
     const { granted } = await requestForegroundPermissionsAsync();
 
@@ -21,14 +25,6 @@ export default function Mapa() {
   useEffect(() => {
     requestLocationsPermissions();
   }, []);
-
-  const coordenadas = [
-    { latitude: -23.171898582924346, longitude: -45.8159475113561, title: "Pereira Lanches", serial: "12345" },
-    { latitude: -23.171616557695884, longitude: -45.81613711352644, title: "Marmitaria Jerônimoo", serial: "67890" },
-    { latitude: -23.17172230680727, longitude: -45.81550332926822, title: "Adega da Avenidas", serial: "54321" },
-    { latitude: -23.171568866892837, longitude: -45.81582811550731, title: "Espetinho do Jerônimoo", serial: "98765" },
-    { latitude: -23.17181556483889, longitude: -45.81586181915286, title: "Lucia Confecções", serial: "13579" },
-  ];
 
   return (
     <View style={{ flex: 1 }}>
@@ -57,14 +53,28 @@ export default function Mapa() {
               longitudeDelta: 0.010,
             }}
           >
-            {coordenadas.map((coordenada, index) => (
+            {equipmente.map((coordenada, index) => (
               <Marker
                 key={index}
-                coordinate={coordenada}
-                title={`${coordenada.title} (Serial: ${coordenada.serial})`}
+                coordinate={{
+                  latitude: Number(coordenada.latitude),
+                  longitude: Number(coordenada.longitude)
+                }}
+                title={`${coordenada.type} (Serial: ${coordenada.serial})`}
                 description={`Latitude: ${coordenada.latitude}, Longitude: ${coordenada.longitude}`}
               />
             ))}
+
+            {location.coords.latitude && location.coords.longitude && (
+              <Marker
+                coordinate={{
+                  latitude: location.coords.latitude,
+                  longitude: location.coords.longitude,
+                }}
+                title="Minha Localização"
+                pinColor="blue"
+              />
+            )}
           </MapView>
         )
       )}
