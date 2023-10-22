07450422c993c4ab8a700032dee6354923761244
Diego Silva <diegosilva157890@gmail.com>
2023-10-22 14:02:14 -0300

    Listagem de equipamentos

--- client/src/components/Card/index.tsx
+++ client/src/components/Card/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { TouchableOpacity, Image, Text, ListRenderItem, FlatList } from "react-native"
+import { TouchableOpacity, Image, Text, ListRenderItem, FlatList, View } from "react-native"
 import styles from "./style";
 
 
@@ -22,29 +22,23 @@ const CardEquipmet = ({filter, onPress}:any) => {
     
     return(
         <FlatList
-          data={filter}
-          keyExtractor={(item) => item._id}
-          numColumns={2}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              style={[
-                styles.column,
-                { backgroundColor: item.status ? 'transparent' : 'rgb(248, 248, 259)', opacity: item.status ? 1 : 0.4 },
-              ]}
-              onPress={() => onPress(item._id)}
-            >
-              <Image
-                source={{ uri: item.url[0] }}
-                style={[
-                  styles.image,
-                  { opacity: item.status ? 1 : 0.3}, 
-                ]}
-              />
-              <Text style={[styles.textfont, { opacity: item.status ? 1 : 0.3}]}>{item.type}</Text>
-              <Text style={{ opacity: item.status ? 1 : 0.3}}>{item.serial}</Text>
-            </TouchableOpacity>
-          )}
-        />
+        data={filter}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) => (
+          <TouchableOpacity style={[styles.card,{ backgroundColor: item.status == 0 ? '#b3b1b1' : '#f0fafc' }]} onPress={() => onPress(item._id)}>
+            <Image source={{ uri: item.url[0] }} style={styles.image} />
+            <View style={styles.textContainer}>
+              <Text style={styles.textfont}>{item.type}</Text>
+              <Text style={styles.textSub}>{item.serial}</Text>
+              {item.status == 0 && (
+              <View style={[styles.containerStatus, { backgroundColor: '#fc3f3f' }]}>
+                <Text style={styles.textStatus}>DESATIVADO</Text>
+              </View>
+            )}
+            </View>
+          </TouchableOpacity>
+        )}
+      />
     )
 }
 
--- client/src/components/ListagemEquipamento/style.ts
+++ client/src/components/ListagemEquipamento/style.ts
@@ -11,44 +11,14 @@ const styles = StyleSheet.create({
     flex: 1,
     marginBottom: 2,
   },
-  row: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: 1,
-  },
-  column: {
-    flex: 1,
-    marginRight: 5,
-    alignItems: "center",
-    paddingTop: 15,
-    marginTop: 10,
-    borderRadius: 5,
-    marginLeft: 5,
-    marginBottom: 5
-    
-  },
-  column2: {
-    flex: 1,
-    marginLeft: 5,
-    alignItems: "center",
-    paddingTop: 15
-  },
-  image: {
-    width: "100%",
-    height: 100,
-    resizeMode: "contain",
-  },
-  textfont: {
-    fontWeight: "bold",
-  },
   uploadingAnimation: {
     position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
-    zIndex: 999, // Isso coloca a animação na frente de outros elementos
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Um fundo semi-transparente para escurecer a tela de fundo
+    zIndex: 999, 
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', 
     justifyContent: 'center',
     alignItems: 'center',
   },
