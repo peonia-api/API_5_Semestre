89c908153cf1576614290b32cfce2e2ea9e24d04
Mateus Silva <mateushls01@gmail.com>
2023-11-08 15:04:11 -0300

    Update api.ts

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://172.190.86.30:3001',
+    baseURL: 'http://40.76.189.93:3001',
     timeout: 1000,
     withCredentials: true,
     headers: {
